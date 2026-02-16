b958176c86149c9b331abbc0f2351bd046cec0ed
Dylano2810 <bakmeijerdylano@gmail.com>
2026-02-16 09:44:56 +0100

    Finishline script en Finishline prefab

---
+++ CrashCourse/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.tag =="Player")
+        {
+            SceneManager.LoadScene("EndScreen");
+        }
+    }
+
+}
