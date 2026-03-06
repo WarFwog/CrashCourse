5e204e4f722ad2c6481ae16ac021c678f0fe7de7
Dylano2810 <bakmeijerdylano@gmail.com>
2026-03-06 13:14:33 +0100

    Loading screens added

--- CrashCourse/Assets/Scripts/LobbyButton.cs
+++ CrashCourse/Assets/Scripts/LobbyButton.cs
@@ -6,7 +6,8 @@ public class LobbyButton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public void Play()
     {
-        SceneManager.LoadScene("LevelScreen");
+        LoadingScreenManager.Instance.SwitchToScene(4);
+        //SceneManager.LoadScene("LevelScreen");
     }
 
     public void Return()
