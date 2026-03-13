8f83c0469b5d627123c6187b2ac2500a924af901
Brian de Reus <33812@ma-web.nl>
2026-03-13 12:31:15 +0100

    rolling logs

---
+++ CrashCourse/Assets/Scripts/TrapLogKnockback.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TrapLogKnockback : TrapKnockback
+{
+    [SerializeField] private Vector3 worldDirection = Vector3.back;
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.LookRotation(worldDirection);
+    }
+}
